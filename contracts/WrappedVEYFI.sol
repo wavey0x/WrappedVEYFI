262276a6ee8386cd7063113be0341b3b79d3b3a0
wavey0x <retrowaveyy@gmail.com>
2022-12-05 09:34:02 -0500

    chore: cleanup

--- contracts/WrappedVEYFI.sol
+++ contracts/WrappedVEYFI.sol
@@ -12,57 +12,6 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-library Address {
-    function isContract(address account) internal view returns (bool) {
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
-        return (codehash != 0x0 && codehash != accountHash);
-    }
-    function toPayable(address account) internal pure returns (address payable) {
-        return payable(address(uint160(account)));
-    }
-    function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
-
-        // solhint-disable-next-line avoid-call-value
-        (bool success, ) = recipient.call{value: amount}("");
-        require(success, "Address: unable to send value, recipient may have reverted");
-    }
-}
-
-library SafeERC20 {
-    using Address for address;
-
-    function safeTransfer(IERC20 token, address to, uint256 value) internal {
-        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
-    }
-
-    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
-    }
-
-    function safeApprove(IERC20 token, address spender, uint256 value) internal {
-        require((value == 0) || (token.allowance(address(this), spender) == 0),
-            "SafeERC20: approve from non-zero to non-zero allowance"
-        );
-        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
-    }
-    function callOptionalReturn(IERC20 token, bytes memory data) private {
-        require(address(token).isContract(), "SafeERC20: call to non-contract");
-
-        // solhint-disable-next-line avoid-low-level-calls
-        (bool success, bytes memory returndata) = address(token).call(data);
-        require(success, "SafeERC20: low-level call failed");
-
-        if (returndata.length > 0) { // Return data is optional
-            // solhint-disable-next-line max-line-length
-            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
-        }
-    }
-}
-
 
 interface VoteEscrow {
     struct LockedBalance {
@@ -76,8 +25,6 @@ interface VoteEscrow {
 }
 
 contract WrappedVEYFI {
-    using SafeERC20 for IERC20;
-    using Address for address;
     
     address constant public YFI = address(0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e);
     
@@ -89,7 +36,7 @@ contract WrappedVEYFI {
     
     constructor() {
         owner = msg.sender;
-        IERC20(YFI).safeApprove(VEYFI, type(uint).max);
+        IERC20(YFI).approve(VEYFI, type(uint).max);
     }
     
     function setController(address _controller) external {
@@ -119,9 +66,13 @@ contract WrappedVEYFI {
         return IERC20(YFI).balanceOf(address(this));
     }
     
-    function transferOwnership(address _owner) external {
+    function transferOwnership(address _owner, bool keepCondition) external {
         require(msg.sender == owner || msg.sender == conditionContract, "!owner");
         owner = _owner;
+        /// @dev reset this after each transfer so that condition becomes unavailable
+        if (!keepCondition){
+            conditionContract = address(0);
+        }
     }
 
     function setTransferCondition(address _conditionContract) external {
