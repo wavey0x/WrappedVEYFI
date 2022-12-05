d0372d30ad468f5373356e0ec1d33be0c8fa16b3
wavey0x <retrowaveyy@gmail.com>
2022-12-05 11:02:31 -0500

    feat: allow operator change on ownership transfer

--- contracts/WrappedVEYFI.sol
+++ contracts/WrappedVEYFI.sol
@@ -27,9 +27,7 @@ interface VoteEscrow {
 contract WrappedVEYFI {
     
     address constant public YFI = address(0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e);
-    
     address constant public VEYFI = address(0x90c1f9220d90d3966FbeE24045EDd73E1d588aD5);
-    
     address public owner;
     address public conditionContract;
     address public controller;
@@ -37,11 +35,6 @@ contract WrappedVEYFI {
     constructor() {
         owner = msg.sender;
         IERC20(YFI).approve(VEYFI, type(uint).max);
-    }
-    
-    function setController(address _controller) external {
-        require(msg.sender == owner, "!owner");
-        controller = _controller;
     }
     
     function modifyLock(uint _amount, uint _unlockTime) external {
@@ -62,19 +55,22 @@ contract WrappedVEYFI {
         }
     }
     
-    function balanceOfWant() public view returns (uint) {
-        return IERC20(YFI).balanceOf(address(this));
+    /// @notice transfer ownership of controller
+    /// @dev this is a sensitive function. Controller has complete access to make calls on behalf of wrapper.
+    function setController(address _controller) external {
+        require(msg.sender == owner, "!owner");
+        controller = _controller;
     }
-    
+
     /// @notice transfer ownership of veYFI position
     /// @param _owner new owner
-    /// @param _resetCondition specify whether or not to reset an existing conditionContract
-    function transferOwnership(address _owner, bool _resetCondition) external {
+    /// @param _newConditionContract specify a new condition contract. 0x0 is a safe default.
+    /// @param _newOperator specify a new operator. 0x0 is a safe default.
+    function transferOwnership(address _owner, address _newConditionContract, address _newOperator) external {
         require(msg.sender == owner || msg.sender == conditionContract, "!owner");
         owner = _owner;
-        if (_resetCondition){
-            conditionContract = address(0);
-        }
+        conditionContract = _newConditionContract;
+        operator = _newOperator;
     }
 
     function setTransferCondition(address _conditionContract) external {
